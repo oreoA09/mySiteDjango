2939c5b3be57276a716708fff37253eb9cdf634c
oreoA09 <oreo.akanni@gmail.com>
2022-06-18 23:53:17 +0000

    enforcing migration to new model

--- I4G008040APG/models.py
+++ I4G008040APG/models.py
@@ -21,5 +21,9 @@ class Country(models.Model):
     name = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+        
     def __str__(self):
         return self.name
